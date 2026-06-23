f15bfb3b4eb10045ceb9a631d1b6c4eb0ffe929a
Mark Ureta <mark.thomas.ureta@gmail.com>
2026-06-23 18:13:24 +1000

    stuff

--- src/main/java/com/weave/Main.java
+++ src/main/java/com/weave/Main.java
@@ -1,8 +1,21 @@
 package com.weave;
 
+import com.weave.models.User;
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to Weave!");
+        System.out.println("Welcome to Weave!\n");
+
+        String name;
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Username: ");
+            name = scanner.nextLine();
+            System.out.print("Password: ");
+            String password = scanner.nextLine();
+        }
+        System.out.println("Welcome, " + name + ".");
     }
 }
